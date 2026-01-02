cc006bdf51b590305e586457b3a816ffda704fe0
Álvaro <alvarob.alano@hotmail.com>
2026-01-02 19:23:29 -0300

    feat: configura api url dinamica para deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+// "Truque" de mestre:
+// Se existir uma variável de ambiente (na Vercel), usa ela.
+// Se não existir (no seu PC), usa o localhost padrão.
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/v1', // Confirme se sua porta é 8000
+    baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/v1',
 });
 
 export default api;
