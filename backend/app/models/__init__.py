5859dfe2c0150b081f41650abe8aa47dc525e17b
Álvaro <alvarob.alano@hotmail.com>
2026-01-06 17:37:23 -0300

    Fix: Remove importacao do arquivo user.py deletado

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,12 @@
-# Importamos os modelos individuais para que o Alembic e o Base 'vejam' eles
-from .user import User
-# Trazendo tudo do tables.py para expor ao sistema
-from .tables import User, Account, Category, Transaction, TransactionType, CreditCard, Goal, Debtor, CreditCardBill
+# Importa todos os modelos do tables.py para expor ao sistema
+from .tables import (
+    User, 
+    Account, 
+    Category, 
+    Transaction, 
+    TransactionType, 
+    CreditCard, 
+    Goal, 
+    Debtor, 
+    CreditCardBill
+)
